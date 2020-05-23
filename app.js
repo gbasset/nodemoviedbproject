47b49eb2148514b8ec7f47481505f2792e502807
gaetan <gbassetprotonmail.com>
2020-05-23 17:14:38 +0200

    add form multer

--- app.js
+++ app.js
@@ -1,20 +1,57 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const multer = require('multer');
+const upload = multer()
 const PORT = 3000
 app.set('views', './Views')
 // template engine on precise où se trouvent les vues
 app.set('view engine', 'ejs')
 // Use pour ajouter le Middleware pour les fichiers statics css / img pdf ...
 app.use('/public', express.static('public'))
+// app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('index')
 })
-
+let frenchMovies = [
+    { title: "Le fabuleux destin d'Amelie Poulain", year: 2001 },
+    { title: "Buffet Froid", year: 1979 },
+    { title: "Le dinet de cons", year: 1998 },
+    { title: "De Rouille et d'os", year: 2012 },
+]
 app.get('/movies', (req, res) => {
-    res.render('movies')
+    const title = 'Films français des 30 dernières années'
+    frenchMovies = [
+        { title: "Le fabuleux destin d'Amelie Poulain", year: 2001 },
+        { title: "Buffet Froid", year: 1979 },
+        { title: "Le dinet de cons", year: 1998 },
+        { title: "De Rouille et d'os", year: 2012 },
+    ]
+    res.render('movies', { movies: frenchMovies, title: title })
 })
-
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// app.post('/movies', urlencodedParser, (req, res) => {
+//     console.log(req.body)
+//     console.log('titre', req.body.movietitle)
+//     const newMovie = { title: req.body.movietitle, year: req.body.movieyear }
+//     frenchMovies = [...frenchMovies, newMovie]
+//     console.log(frenchMovies);
+//     res.sendStatus(201)
+// })
+app.post('/movies', upload.fields([]), (req, res) => {
+    if (!req.body) {
+        res.sendStatus(500)
+    } else {
+        console.log(req.body)
+        const formData = req.body
+        console.log("formData", formData);
+        const newMovie = { title: req.body.movietitle, year: req.body.movieyear }
+        frenchMovies = [...frenchMovies, newMovie]
+        console.log(frenchMovies);
+        res.sendStatus(201)
+    }
+})
 app.get('/movies/add', (req, res) => {
     // res.send('créer votre fiche film !')
     res.render('addMovie')
